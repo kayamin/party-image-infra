8e2c396967e39cd4fb639c9739a4bf3d4cd611d8
Atsushi Kayama <a.shika829@gmail.com>
2021-01-14 14:15:47 +0900

    sqs を追加，アプリケーションで必要な権限を eksのノードグループに付与

--- terraform/shared/modules/service/aws_iam_eks_node_group.tf
+++ terraform/shared/modules/service/aws_iam_eks_node_group.tf
@@ -32,3 +32,26 @@ resource "aws_iam_role_policy_attachment" "eks-cluster-node-role-AmazonEC2Contai
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks-cluster-node-role.name
 }
+
+// アプリケーションで必要な権限を付与, S3, Dynamo, Rekognition, SQS
+resource "aws_iam_role_policy_attachment" "s3-full" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.eks-cluster-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb-full" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  role       = aws_iam_role.eks-cluster-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "rekognition-full" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRekognitionFullAccess"
+  role       = aws_iam_role.eks-cluster-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "sqs-full" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+  role       = aws_iam_role.eks-cluster-node-role.name
+}
+
+
